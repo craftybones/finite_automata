1c3c0ad1b5e3f2242cd907086d3d3cb6056c020c
Srijayanth Sridhar <srijayanth@gmail.com>
2016-06-04 12:54:47 +0530

    Renamed test to DFA space

--- test/finite_automata/dfa_test.clj
+++ test/finite_automata/dfa_test.clj
@@ -1,4 +1,4 @@
-(ns finite-automata.core-test
+(ns finite-automata.core-dfa-test
   (:require [clojure.test :refer :all]
             [finite-automata.core :refer :all]))
 
@@ -23,6 +23,17 @@
    :start-state :q1
    :final-states #{:q2}})
 
+(def even-number-of-zeroes
+  {:delta {:q1 {1 :q1 0 :q2} :q2 {1 :q2 0 :q1}}
+   :start-state :q1
+   :final-states #{:q1}})
+
+(def ends-with-one
+  {:delta {:q1 {1 :q1 0 :q2} :q2 {1 :q1 0 :q2}}
+   :start-state :q1
+   :final-states #{:q1}})
+
+
 (deftest dfa-test
   (testing "single state dfa"
     (testing "any string"
@@ -37,4 +48,22 @@
         (is (true? (d "0")))
         (is (false? (d "1")))
         (is (true? (d "10")))
-        (is (false? (d "11")))))))
+        (is (false? (d "11")))))
+    (testing "even number of zeroes"
+      (let [d (dfa-of even-number-of-zeroes)]
+        (is (true? (d "")))
+        (is (true? (d "00")))
+        (is (true? (d "0000")))
+        (is (true? (d "1001")))
+        (is (true? (d "1100")))
+        (is (true? (d "10101")))
+        (is (true? (d "001100")))
+        (is (false? (d "0")))
+        (is (false? (d "000")))
+        (is (false? (d "1000")))
+        (is (false? (d "01010")))))
+    (testing "ends with one"
+      (let [d (dfa-of ends-with-one)]
+        (is (true? (d "1")))
+        (is (true? (d "01")))
+        (is (false? (d "0")))))))
