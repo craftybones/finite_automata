ec7944c16c7a6764a749f4490eb2edbaf6000a58
Srijayanth Sridhar <srijayanth@gmail.com>
2016-06-09 19:47:29 +0530

    Added a loopy version of an existing test which supposedly breaks a bunch of things

--- examples.clj
+++ examples.clj
@@ -289,6 +289,39 @@
                "1010"]
   }
  {
+  :name "0*1* or 1*0* with extra epsilons"
+  :type "nfa"
+  :tuple {
+          :states #{:q1 :q2 :q3 :q4 :q5 :q6}
+          :alphabets #{"0" "1"}
+          :delta {:q1 {:e #{:q2 :q4}}
+                  :q2 {0 #{:q2} :e #{:q3}}
+                  :q3 {1 #{:q3} :e #{:q6}}
+                  :q4 {1 #{:q4} :e #{:q5}}
+                  :q5 {0 #{:q5} :e #{:q7}}
+                  }
+          :start-state :q1
+          :final-states #{:q6 :q7}
+          }
+  :pass-cases [""
+               "0"
+               "1"
+               "00"
+               "11"
+               "001"
+               "110"
+               "011"
+               "100"
+               "0011"
+               "1100"]
+  :fail-cases ["101"
+               "010"
+               "11001"
+               "00110"
+               "0101"
+               "1010"]
+  }
+ {
   :name "001* or 101*"
   :type "nfa"
   :tuple {
